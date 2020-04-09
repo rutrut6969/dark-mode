c9df4cfa4f223ecc3f007f8086436b9e3d9e95ce
rutrut6969 <rutledgeisaac6969@gmail.com>
2020-04-08 19:00:39 -0500

    finished comps, project working

--- src/index.js
+++ src/index.js
@@ -1,30 +1,31 @@
-import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
 
-import Charts from "./components/Charts";
-import Navbar from "./components/Navbar";
+import Charts from './components/Charts';
+import Navbar from './components/Navbar';
 
-import "./styles.scss";
+import './styles.scss';
 
 const App = () => {
   const [coinData, setCoinData] = useState([]);
-
+  // const body = document.querySelector('body');
+  // console.log(body);
   useEffect(() => {
     axios
       .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true'
       )
-      .then(res => setCoinData(res.data))
-      .catch(err => console.log(err));
+      .then((res) => setCoinData(res.data))
+      .catch((err) => console.log(err));
   }, []);
   return (
-    <div className="App">
+    <div className='App'>
       <Navbar />
       <Charts coinData={coinData} />
     </div>
   );
 };
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
